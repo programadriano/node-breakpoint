e137d377a6b2e1f7648e150867a57e2e5059793a
Thiago Adriano <tadriano@band.com.br>
2017-08-26 13:31:12 -0300

    adicionando metodo com parametro

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
+// grab the packages we need
+var express = require('express');
+var app = express();
+var port =  3030;
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.get('/api/users', function (req, res) {
+    var user_id = req.param('id');
+    var name = req.param('name');
+
+    res.send(user_id + ' ' + name);
+});
+
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
